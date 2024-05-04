490ad23c16686a0d462a01fb8b76e5b2608e97f8
sajjad-mugdho <sajjadmugdho@yahoo.com>
2024-05-04 18:06:30 +0600

    [Test]

--- config/database.js
+++ config/database.js
@@ -1,17 +1,18 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const DB_HOST = process.env.DB_HOST
-const DB_PORT = process.env.DB_PORT
-const DB_DATABASE = process.env.DB_DATABASE
-const DB_USERNAME = process.env.DB_USERNAME
-const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_DATABASE = process.env.DB_DATABASE;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 // database connection
 const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-  dialect: 'mysql',
+  dialect: "mysql",
+  dialectModule: require("mysql2"),
   host: DB_HOST,
-  logging: false
+  logging: false,
 });
 
 module.exports = sequelize;
